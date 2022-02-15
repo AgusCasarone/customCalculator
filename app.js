81aaabecda0544c323bdd24ca7010d87be02f0db
AgusCasarone <agus.casarone@gmail.com>
2022-02-15 16:59:42 -0300

    rule of three functioning + error 404 updated

--- app.js
+++ app.js
@@ -12,12 +12,44 @@ function theme () {
     }
 }
 
+// MODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTION
+
+let conversionSegment = document.getElementsByClassName('conversion')
+let calculatorSegment = document.getElementsByClassName('calculator')
+let ruleSegment = document.getElementsByClassName('rule')
+console.log('calculatorSegment')
+
+function conversion () {
+    ruleSegment[0].style.visibility = 'hidden';
+    calculatorSegment[0].style.visibility = 'hidden';
+    conversionSegment[0].style.visibility = 'visible';
+    ruleSegment.style.zIndex = '-1';
+    calculatorSegment.style.zIndex = '-1';
+    conversionSegment.style.zIndex = '1';
+}
+
+function rule () {
+    conversionSegment[0].style.visibility = 'hidden';
+    calculatorSegment[0].style.visibility = 'hidden';
+    ruleSegment[0].style.visibility = 'visible';
+    conversionSegment.style.zIndex = '-1';
+    calculatorSegment.style.zIndex = '-1';
+    ruleSegment.style.zIndex = '1';
+}
+
+function calculator () {
+    conversionSegment[0].style.visibility = 'hidden';
+    ruleSegment[0].style.visibility = 'hidden';
+    calculatorSegment[0].style.visibility = 'visible';
+    conversionSegment.style.zIndex = '-1';
+    ruleSegment.style.zIndex = '-1';
+    calculatorSegment.style.zIndex = '1';
+}
+
+
 // CONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSIONCONVERSION
 
 let input = document.getElementById("users-number")
-
-let testParagraphTwo = document.getElementById("test-me-two")
-let testParagraph = document.getElementById("test-me")
 
 let finalResultLength = document.getElementById("length")
 let finalResultVolume = document.getElementById("volume")
@@ -49,26 +81,26 @@ input.oninput = function updateValues() {
     finalResultMass.textContent = input.value + " kilos = " + resultMetricToAmericanMass.toFixed(3) + " pounds | " + input.value + " pounds = " + resultAmericanToMetricMass.toFixed(3) + " kilos"
 }
 
-// MODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTIONMODESELECTION
+// RULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULERULE
 
-let conversionButton = document.getElementsByClassName('conversion')
-let calculatorButton = document.getElementsByClassName('calculator')
-let ruleButton = document.getElementsByClassName('rule')
+let firstNumber = document.getElementById("first-number")
+let secondNumber = document.getElementById("second-number")
+let thirdNumber = document.getElementById("third-number")
+let finalResult = document.getElementById("result")
 
-function conversion () {
-    document.getElementsByClassName('conversion')[0].style.visibility = 'visible';
-    document.getElementsByClassName('rule')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('calculator')[0].style.visibility = 'hidden';
-}
+let explanationFirstNumber = document.getElementById("explanation-first-number")
+let explanationSecondNumber = document.getElementById("explanation-second-number")
+let explanationThirdNumber = document.getElementById("explanation-third-number")
+let explanationResult = document.getElementById("explanation-result-number")
 
-function rule () {
-    document.getElementsByClassName('rule')[0].style.visibility = 'visible';
-    document.getElementsByClassName('conversion')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('calculator')[0].style.visibility = 'hidden';
-}
+let result = 0
 
-function calculator () {
-    document.getElementsByClassName('calculator')[0].style.visibility = 'visible';
-    document.getElementsByClassName('conversion')[0].style.visibility = 'hidden';
-    document.getElementsByClassName('rule')[0].style.visibility = 'hidden';
+thirdNumber.oninput = function ruleOfThree() {
+    result = thirdNumber.value * secondNumber.value
+    result = result / firstNumber.value
+    finalResult.innerText = result
+    explanationFirstNumber.innerText = firstNumber.value
+    explanationSecondNumber.innerText = secondNumber.value
+    explanationThirdNumber.innerText = thirdNumber.value
+    explanationResult.innerText = result
 }
